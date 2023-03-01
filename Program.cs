3ab5cc8620f8e72bc7b5dc3c976c9ad7643aa77f
PC04-204 <PC04-204@DESKTOP-Q8235VD>
2023-03-01 15:15:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace deterinanta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = Matrix<double>.Build.DenseOfArray(new double[,]{
+                 { 0, 1, 1, 2},
+                 { 2 , 1, 2, -1},
+                 { 1, -1, 3, -2},
+                 { 3, 2, -1, 4}
+                  });
+            var b = Vector<double>.Build.Dense(new double[]
+                { -3, 6 , 10 , -15});
+            var x = A.Solve(b);
+
+            Console.WriteLine(x);
+        }
+    }
+}
